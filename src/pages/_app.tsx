6495f10fef0130a47b4931c1c5f2d8ffd0854661
Ribbit <echo07200503@gmail.com>
2022-06-09 16:35:59 +0900

    ✨ add tailwind css and remove styled component.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import type { AppProps } from 'next/app';
 import 'src/styles/reset.css';
+import 'src/styles/global.css';
 
 const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
 
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ['./src/pages/**/*.{ts,tsx}', './src/components/**/*.{ts,tsx}'],
+  theme: {
+    extend: {
+      boxShadow: {
+        glass: '0 1px 3px -1px #0002, 0 3px 5px -2px #0003, inset 2px 2px 3px -1px #fffc',
+        'glass-hover': '0 3px 5px -1px #0002, 0 8px 15px -5px #0003, inset 2px 2px 3px -1px #fffc',
+      },
+      keyframes: {
+        float: {
+          '0%': { transform: 'translate(0, 0) rotate(45deg)' },
+          '100%': { transform: 'translate(0, 10%) rotate(60deg)' },
+        },
+      },
+      animation: {
+        float: 'float 5s ease-in-out 0s infinite alternate',
+        float2: 'float 5s ease-in-out -2s infinite alternate',
+        float4: 'float 5s ease-in-out -4s infinite alternate',
+      },
+    },
+  },
+  plugins: [require('tailwind-clip-path')],
+};
