dc800c64750f70743d7e92ac1a5642929883b2d5
Ribbit <echo07200503@gmail.com>
2022-06-11 15:04:50 +0900

    ✨ add GTM component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,13 @@ import type { AppProps } from 'next/app';
 import 'src/styles/reset.css';
 import 'src/styles/global.css';
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+import { GoogleTagManager } from 'src/components/functional/google-tag-manager';
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <GoogleTagManager />
+    <Component {...pageProps} />
+  </>
+);
 
 export default App;
